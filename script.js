10a5d09cf200bfe43c8b753e06a90eb6de65c217
IrinaNik05 <irinashik0510@gmail.com>
2024-01-13 15:05:30 +0300

    домашнее задание 5

--- script.js
+++ script.js
@@ -13,3 +13,56 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const forecast = document.querySelector('.current-forecast h1');
+const probability = document.querySelector('.current-forecast p');
+const button = document.querySelector('.forecast-btn');
+
+
+
+function saveNewForecast(forecast, probability) {
+    const forecastItem = document.querySelector('#forecast-item');
+    const cardWithForecast = forecastItem.content.cloneNode(true);
+
+    cardWithForecast.querySelector('.forecast-item h3').textContent = forecast;
+    cardWithForecast.querySelector('.forecast-item p').textContent = probability;
+
+    const forecastContainer = document.querySelector('.forecasts');
+    forecastContainer.append(cardWithForecast);
+}
+
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+button.addEventListener("click", function() {
+    const number = getRandomInt(0, 5);
+
+    forecast.textContent = getRandomInt(0, 5);
+    switch (number) {
+        case 0:
+            forecast.textContent = 'У тебя все получится!';
+            break;
+        case 1:
+            forecast.textContent = 'Тебя ждут хорошие новости.';
+            break;
+        case 2:
+            forecast.textContent = 'Сегодня замечательный день, чтобы сходить в кино.';
+            break;
+        case 3:
+            forecast.textContent = 'Отличный день, чтобы купить новую сумочку.';
+            break;
+        case 4:
+            forecast.textContent = 'Скоро все изменится.';
+            break;
+        case 5:
+            forecast.textContent = 'Самое время что-то изменить.';
+            break;
+
+    }
+    probability.textContent = `Вероятность: ${getRandomInt(0, 100)}%`;
+
+    saveNewForecast(forecast.textContent, probability.textContent);
+})
